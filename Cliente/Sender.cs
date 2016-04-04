6a0f0803fa73b65d7712b6aaddf15bc4c0e83c96
maximoreor14 <maximorero14@gmail.com>
2016-04-04 13:46:54 -0300

    cracion clase listener

---
+++ Cliente/Sender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Sender
+    {
+        StreamReader reader;
+        StreamWriter writer;
+        int port;
+        String ip;
+        TcpClient client;
+
+        public Sender(int port, String ip)
+        {
+            this.port = port;
+            this.ip = ip;
+        }
+        public void escribir()
+        {
+
+        }
+        
+    }
+}
