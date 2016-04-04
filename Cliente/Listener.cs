210ef1b62235af01e5e15c2ce67e9c4e46dd8094
maximoreor14 <maximorero14@gmail.com>
2016-04-04 13:45:28 -0300

    creacion clase listener

--- Cliente/Listener.cs
+++ Cliente/Listener.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,35 @@ namespace Cliente
 {
     class Listener
     {
+        StreamReader reader;
+        StreamWriter writer;
+        int port;
+        String ip;
+        TcpClient client;
+
+        public Listener(int port, String ip)
+        {
+            this.port = port;
+            this.ip = ip;
+        }
+
+        public void escuchar()
+        {
+            client = new TcpClient(ip, port);
+            NetworkStream stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream) { AutoFlush = true };
+
+            while (client.Connected)
+            {
+                string lineReceived = reader.ReadLine();
+                Console.WriteLine("El servidor respondio: " + lineReceived);
+                /*TIRAR EVENTO*/
+            }
+            writer.Close();
+            reader.Close();
+            stream.Close();
+            client.Close();
+        }
     }
 }
